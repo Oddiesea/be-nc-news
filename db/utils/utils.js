1f5f7531f66c07eed14732a03ef939fc3c2a4f51
Oddiesea <liamwiltonjones@gmail.com>
2019-09-23 12:58:54 +0100

    Work on utility function formatDates and testing.

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,16 @@
-exports.formatDates = list => {};
+const _ = require('lodash')
+
+exports.formatDates = list => {
+    const listCopy = _.cloneDeep(list);
+    listCopy.forEach(element => {
+                let timestamp = new Date(element.created_at)
+                let formattedDate = timestamp.toJSON().toString();
+                formattedDate = formattedDate.replace('T', ' ')
+                console.log(formattedDate)
+              element.created_at = formattedDate;
+        });
+        return listCopy;
+      };
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,7 +5,71 @@ const {
   formatComments,
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  it('returns an empty array when passed one', () => {
+    expect(formatDates([])).to.eql([]);
+  });
+  it('doesn\'t change the contents of the input array' , () => {
+    const input = [{
+      body: 'Test body.',
+      belongs_to: 'Test Title',
+      created_by: 'testUser',
+      votes: -1,
+      created_at: 1468087638932,
+    },
+    {
+      body: 'Another test body.',
+      belongs_to: 'Test Title 2',
+      created_by: 'otherTestUser',
+      votes: 7,
+      created_at: 1478813209256,
+    }]
+    const inputCopy = [{
+      body: 'Test body.',
+      belongs_to: 'Test Title',
+      created_by: 'testUser',
+      votes: -1,
+      created_at: 1468087638932,
+    },
+    {
+      body: 'Another test body.',
+      belongs_to: 'Test Title 2',
+      created_by: 'otherTestUser',
+      votes: 7,
+      created_at: 1478813209256,
+    }]
+    formatDates(input);
+    expect(input).to.eql(inputCopy)
+  });
+  it('doesn\'t reference any objects from the original array', () => {
+    const input = [{
+      body: 'Test body.',
+      belongs_to: 'Test Title',
+      created_by: 'testUser',
+      votes: -1,
+      created_at: 1468087638932,
+    },
+    {
+      body: 'Another test body.',
+      belongs_to: 'Test Title 2',
+      created_by: 'otherTestUser',
+      votes: 7,
+      created_at: 1478813209256,
+    }]
+    expect(formatDates(input)[0]).to.not.equal(input[0])
+    expect(formatDates(input)).to.not.equal(input)
+  });
+  it('returns an array of a single item with the date re-formatted from unix timestamp', () => {
+    const input = [{
+      body: 'Test body.',
+      belongs_to: 'Test Title',
+      created_by: 'testUser',
+      votes: -1,
+      created_at: 1468087638932,
+    }]
+    expect(formatDates(input).created_at).to.equal(input)
+  });
+});
 
 describe('makeRefObj', () => {});
 
