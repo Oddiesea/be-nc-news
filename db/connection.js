f4a1105519e6002e8fd1056d5673d3a181e97caa
Oddiesea <liamwiltonjones@gmail.com>
2019-09-26 16:53:02 +0100

    More knexfile updates for hosting.

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
+const ENV = process.env.NODE_ENV || "development";
 const dbConfig =
   ENV === 'production'
     ? { client: 'pg', connection: process.env.DATABASE_URL }
     : require('../knexfile');
-    
+
 const connection = require('knex')(dbConfig);
 
 
